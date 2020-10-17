9fd26d8f869fdf506becf6456d8ac73766991028
Xeno <xeno.song@outlook.com>
2020-10-17 20:06:41 +0900

    Remove all program file to rebuild 3d engine

--- Pioneer/Mmath.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-//#define	min(_n, _m)	((_n > _m)? _m : _n)
-//#define	max(_n, _m)	((_n > _m)? _n : _m)
-
-#include <math.h>
-
-#include "cMmathVector3.h"
-#include "cMmathRect.h"
--- Pioneer/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "MEngine.h"
-#include <Windows.h>
-
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
-{
-	MGetSystem()->InitEngine(hInstance);
-	//MGetSystem()->StartEngine();
-	MGetSystem()->GetWindowManager()->Destroy();
-}
