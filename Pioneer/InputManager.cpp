29b0bfce1c23b64052a4784256c82f929107b574
Xeno <xeno.song@outlook.com>
2021-01-28 02:43:59 +0900

    [ADD] Create input manager class

---
+++ Pioneer/InputManager.cpp
@@ -0,0 +1,63 @@
+#include "InputManager.h"
+
+namespace pioEngine
+{
+	cInputManager::cInputManager()
+	{
+		for (int i = 0; i < 256; i++)
+			m_keyStateMemory[i] = false;
+	}
+
+	cInputManager::cInputManager(cInputManager & origin)
+	{
+		for (int i = 0; i < 256; i++)
+		{
+			m_keyStateMemory[i] = origin.GetKeyState(i);
+		}
+
+		m_cursorPosition = origin.GetCursorPosition();
+	}
+
+	cInputManager::~cInputManager()
+	{
+	}
+
+	void cInputManager::SetKeyState(unsigned char key, bool state)
+	{
+		m_keyStateMemory[key] = state;
+	}
+
+	bool cInputManager::GetKeyState(unsigned char key)
+	{
+		return m_keyStateMemory[key];
+	}
+
+	void cInputManager::SetCursorPosition(POINT cursor)
+	{
+		m_cursorPosition = cursor;
+	}
+
+	POINT cInputManager::GetCursorPosition()
+	{
+		return m_cursorPosition;
+	}
+
+	bool cInputManager::GetKeyStateAsync(unsigned char key)
+	{
+		bool result;
+		result = GetAsyncKeyState(key);
+
+			return false;
+	}
+
+	POINT cInputManager::GetCursorPositionAsync()
+	{
+		LPPOINT point;
+		
+		if (GetCursorPos(point) == false)
+			return POINT{ 0, 0 };
+
+		return *point;
+	}
+
+}
---
+++ Pioneer/InputManager.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#ifndef __C_INPUT_MANAGER_H__
+#define __C_INPUT_MANAGER_H__
+
+#include <Windows.h>
+
+namespace pioEngine
+{
+	class cInputManager
+	{
+	public:
+		cInputManager();
+		cInputManager(cInputManager&);
+
+		virtual ~cInputManager();
+
+	public:
+		void SetKeyState(unsigned char key, bool state);
+		bool GetKeyState(unsigned char key);
+
+		void SetCursorPosition(POINT cursor);
+		POINT GetCursorPosition();
+		
+		bool GetKeyStateAsync(unsigned char key);
+		POINT GetCursorPositionAsync();
+
+	private:
+		bool m_keyStateMemory[256] = { false };
+		POINT m_cursorPosition;
+	};
+}
+
+#endif
